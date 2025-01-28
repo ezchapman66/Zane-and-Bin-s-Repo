cbf660549fdd5591280d310c5a76a6a11b58dfa8
Bin Dao <bdao81304@gmail.com>
2025-01-28 13:38:19 -0600

    this adds info

--- intro.py
+++ intro.py
@@ -4,3 +4,4 @@ stocks = yf.Ticker("MSFT") #creates a Ticker object from its class
 stock_info = stocks.calendar #uses Ticker's calendar function to return a dictionary of data
 print(stock_info) #this is a dictionary with analytical data
 print(stock_info["Earnings Average"], "\n") #this is how you iterate over a dictionary
+print(stocks.info)
