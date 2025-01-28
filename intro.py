d820d24702758beca9c4f916e78bca894a67fd2a
ezchapman66 <157661074+ezchapman66@users.noreply.github.com>
2025-01-28 13:26:48 -0600

    This introduces yfinance and yf

--- intro.py
+++ intro.py
@@ -1,3 +1,6 @@
-print("Hello world")
+import yfinance as yf
 
-print(8/4)
+stocks = yf.Ticker("MSFT") #creates a Ticker object from its class
+stock_info = stocks.calendar #uses Ticker's calendar function to return a dictionary of data
+print(stock_info) #this is a dictionary with analytical data
+print(stock_info["Earnings Average"], "\n") #this is how you iterate over a dictionary
